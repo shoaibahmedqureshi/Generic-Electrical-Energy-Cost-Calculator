c47aaaeddb35d879b8f40b8d4adb22e3150607f6
Shoaib Ahmed Qureshi <08bitsaqureshi@seecs.edu.pk>
2019-02-23 18:01:59 +0500

    Initial Commit

---
+++ MRCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MRCalculator
+//
+//  Created by Shoaib Ahmed Qureshi on 2/23/19.
+//  Copyright © 2019 Shoaib Ahmed Qureshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
